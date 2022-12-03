8f5f22b25a287880d31b7ee18145f1e18f7e5d80
Francisco Sousa <francisco.sousa1987@outlook.com>
2022-12-03 08:57:24 -0300

    Mapeando e persistindo dados de arquivos com a anotação Lob

--- src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
+++ src/main/java/com/algaworks/ecommerce/model/NotaFiscal.java
@@ -29,7 +29,8 @@ public class NotaFiscal {
 //    )
     private Pedido pedido;
 
-    private String xml;
+    @Lob
+    private byte[] xml;
 
     @Column(name = "data_emissao")
     private Date dataEmissao;
